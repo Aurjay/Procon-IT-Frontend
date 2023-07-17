96af1664612f11100122b41e63d9005f613c6586
Aurjay <ddps628@gmail.com>
2023-07-17 16:36:54 +0200

    latest

--- pages/model_results/index.js
+++ pages/model_results/index.js
@@ -32,7 +32,7 @@ const useStyles = makeStyles({
     width: "125%", // Increase the width of the card
     margin: "20px auto", // Adjust the margin as needed
     padding: 10,
-    },
+  },
   statusCritical: {
     color: "red",
     fontWeight: "bold",
@@ -82,91 +82,102 @@ export default function Home() {
         </CardContent>
       </Card>
 
-      {results && (
-        <Box className={classes.resultCard}>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" component="h3" gutterBottom>
-                Overall Metrics
-              </Typography>
-              <Table>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <Typography variant="body1">Mean Absolute Error</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">{results.Overall_metrics.mean_absolute_error}</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <Typography variant="body1">Mean Squared Error</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">{results.Overall_metrics.mean_squared_error}</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <Typography variant="body1">R-squared</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">{results.Overall_metrics.r_squared}</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <Typography variant="body1">Root Mean Squared Error</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">{results.Overall_metrics.root_mean_squared_error}</Typography>
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-
-          <Card className={classes.resultCard}>
-            <CardContent>
-              <Typography variant="h6" component="h3" gutterBottom>
-                Test Set Samples
-              </Typography>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Sample</TableCell>
-                    <TableCell>Rainflow Counts</TableCell>
-                    <TableCell>Test Label</TableCell>
-                    <TableCell>Prediction</TableCell>
-                    <TableCell>Status</TableCell>
-                    <TableCell>Time to Failure</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Object.keys(results.test_set_samples).map((sampleKey, index) => {
-                    const sample = results.test_set_samples[sampleKey];
-                    const isStatusCritical = sample.status === "Status Critical";
-                    const statusClass = isStatusCritical ? classes.statusCritical : classes.statusNotCritical;
-                    const rowHeading = index === 0 ? `${sampleKey} (First->smaller rainflow)` : index === 1 ? `${sampleKey} (Middle)` : `${sampleKey} (Last->higher rainflow)`;
-
-                    return (
-                      <TableRow key={sampleKey}>
-                        <TableCell>{rowHeading}</TableCell>
-                        <TableCell>{sample.rainflow_counts}</TableCell>
-                        <TableCell>{sample.test_label}</TableCell>
-                        <TableCell>{sample.prediction}</TableCell>
-                        <TableCell className={statusClass}>{sample.status}</TableCell>
-                        <TableCell>{sample.time_to_failure}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
+      {isLoading ? (
+        <Box className={classes.loading}>
+          <CircularProgress />
         </Box>
+      ) : (
+        results && (
+          <Box className={classes.resultCard}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" component="h3" gutterBottom>
+                  Overall Metrics
+                </Typography>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="body1">Mean Absolute Error</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="body1">{results.Overall_metrics.mean_absolute_error}</Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="body1">Mean Squared Error</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="body1">{results.Overall_metrics.mean_squared_error}</Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="body1">R-squared</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="body1">{results.Overall_metrics.r_squared}</Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="body1">Root Mean Squared Error</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="body1">{results.Overall_metrics.root_mean_squared_error}</Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+
+            <Card className={classes.resultCard}>
+              <CardContent>
+                <Typography variant="h6" component="h3" gutterBottom>
+                  Test Set Samples
+                </Typography>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Sample</TableCell>
+                      <TableCell>Rainflow Counts</TableCell>
+                      <TableCell>Test Label</TableCell>
+                      <TableCell>Prediction</TableCell>
+                      <TableCell>Status</TableCell>
+                      <TableCell>Time to Failure</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {Object.keys(results.test_set_samples).map((sampleKey, index) => {
+                      const sample = results.test_set_samples[sampleKey];
+                      const isStatusCritical = sample.status === "Status Critical";
+                      const statusClass = isStatusCritical ? classes.statusCritical : classes.statusNotCritical;
+                      const rowHeading =
+                        index === 0
+                          ? `${sampleKey} (First->smaller rainflow)`
+                          : index === 1
+                          ? `${sampleKey} (Middle)`
+                          : `${sampleKey} (Last->higher rainflow)`;
+
+                      return (
+                        <TableRow key={sampleKey}>
+                          <TableCell>{rowHeading}</TableCell>
+                          <TableCell>{sample.rainflow_counts}</TableCell>
+                          <TableCell>{sample.test_label}</TableCell>
+                          <TableCell>{sample.prediction}</TableCell>
+                          <TableCell className={statusClass}>{sample.status}</TableCell>
+                          <TableCell>{sample.time_to_failure}</TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+          </Box>
+        )
       )}
     </Container>
   );
